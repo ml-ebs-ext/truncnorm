1fe52a0d9f71062e1b8015cb2a3ba5c03fa15374
zxBIB Lechón,Miguel (MED BDS) EXTERNAL <miguel.lechon.ext@boehringer-ingelheim.com>
2026-06-08 20:17:45 +0200

    Remove headers and expand macro.

--- src/rtruncnorm.c
+++ src/rtruncnorm.c
@@ -212,23 +212,14 @@ SEXP C_do_rtruncnorm(SEXP s_n, SEXP s_a, SEXP s_b, SEXP s_mean, SEXP s_sd) {
   return s_ret;
 }
 
-#include <stdlib.h>
-#include <R.h>
-#include <Rmath.h>
-#include <Rinternals.h>
-#include <R_ext/Rdynload.h>
-#include <R_ext/Visibility.h>  // optional
-
-#define CALLDEF(name, n)  {#name, (DL_FUNC) &name, n}
-
 extern SEXP C_do_rtruncnorm(SEXP, SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef R_CallDef[] = {
-    CALLDEF(C_do_rtruncnorm, 5),
+    {"C_do_rtruncnorm", (DL_FUNC) &C_do_rtruncnorm, 5},
     {NULL, NULL, 0}
 };
 
-void attribute_visible R_init_truncnorm(DllInfo *dll) {
+void R_init_truncnorm(DllInfo *dll) {
     R_registerRoutines(dll, NULL, R_CallDef, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
     R_forceSymbols(dll, TRUE);
