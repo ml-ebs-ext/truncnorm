1d9fda795bbd1ed9bf1f5908b962d6a35cded1b4
zxBIB Lechón,Miguel (MED BDS) EXTERNAL <miguel.lechon.ext@boehringer-ingelheim.com>
2026-06-09 13:49:09 +0200

    Merge all tests under a single file.

--- tests/testthat/test-rtruncnorm.R
+++ tests/testthat/test-rtruncnorm.R
@@ -1,7 +1,20 @@
-context("sanity checks")
+##
+## Don't segfault!
+##
+
+context("reg-segfault")
+
+expect_error(truncnorm__rtruncnorm(1, numeric(0), 1, 0, 1))
+expect_error(truncnorm__rtruncnorm(1, 0, numeric(0), 0, 1))
+expect_error(truncnorm__rtruncnorm(1, 0, 1, numeric(0), 1))
+expect_error(truncnorm__rtruncnorm(1, 0, 1, 0, numeric(0)))
+
 
 ################################################################################
 ## Sanity checks on random number generators
+
+context("sanity checks")
+
 check_r <- function(a, b, mean, sd, n=10000) {
   prefix <- sprintf("R: a=%f, b=%f, mean=%f, sd=%f", a, b, mean, sd)
   x <- truncnorm__rtruncnorm(n, a, b, mean, sd)
