8f37669841d9a57ed7bba8c390a42b9eedd6606b
zxBIB Lechón,Miguel (MED BDS) EXTERNAL <miguel.lechon.ext@boehringer-ingelheim.com>
2026-06-09 14:44:25 +0200

    Forgo non-ASCII characters.

--- src/rtruncnorm.c
+++ src/rtruncnorm.c
@@ -1,11 +1,11 @@
 /* The contents of this file have been adapted from version 1.0-9 of the {truncnorm} package, retrieved from:
  *   https://github.com/olafmersmann/truncnorm/tree/28593c0b454bdbe860bc00e8e23690c12af89927
  * The package is credited to
- *   Olaf Mersmann, Heike Trautmann, Detlef Steuer and Björn Bornkamp
+ *   Olaf Mersmann, Heike Trautmann, Detlef Steuer and Bjorn Bornkamp
  * and is released under the GNU General Public Licence v2 (or later).
  * The package does not include a copy of that license, so it is not reproduced here.
  *
- * The superficial changes that led to this particular file were performed by Miguel Lechón on behalf of
+ * The superficial changes that led to this particular file were performed by Miguel Lechon on behalf of
  * Boehringer-Ingelheim Pharma GmbH & Co.KG. They do not deviate meaningfully from the original work, so no
  * separate copyright is claimed.
  */
